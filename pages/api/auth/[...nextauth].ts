f4a713bbffb7a16c2aadf80c94b2f17b3a1391c4
Christopher Hærem <chris.haerem@gmail.com>
2023-10-30 18:46:54 +0100

    Added logger to auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,25 @@
-// pages/api/auth/[...nextauth].ts
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import SpotifyProvider from "next-auth/providers/spotify";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const logger = {
+  error(code: string, metadata: any) {
+    console.error(code, metadata);
+  },
+  warn(message: string) {
+    console.warn(message);
+  },
+  debug(message: string) {
+    console.debug(message);
+  },
+};
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
@@ -25,4 +31,5 @@ export default NextAuth({
       },
     }),
   ],
+  logger,
 });

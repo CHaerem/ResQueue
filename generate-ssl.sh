0f9a30e85828d285356c789522f2b3665c1c5a52
Christopher Hærem <chris.haerem@gmail.com>
2023-11-09 14:58:30 +0100

    Fixed ssl script

--- generate-ssl.sh
+++ generate-ssl.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Create the certs directory if it doesn't exist
+mkdir -p ./certs
+
+# Generate SSL keys
 openssl req -x509 -newkey rsa:4096 -keyout ./certs/localhost-key.pem -out ./certs/localhost.pem -days 365 -nodes -subj "/CN=localhost"

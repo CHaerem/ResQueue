5e423687df188e411ce3053d99fc7e349b1404d6
Christopher Hærem <chris.haerem@gmail.com>
2024-05-05 17:56:50 +0200

    chore: Refactor GET function in playlist route to use Fetch API

--- app/api/playlists/route.ts
+++ app/api/playlists/route.ts
@@ -1,24 +1,54 @@
-// app/api/playlists/route.ts
+import { auth } from "@/auth";
 
-import { auth } from "../../../auth";
+interface SpotifyApiError {
+  status: number;
+  message: string;
+}
 
+interface SpotifyPlaylistItem {
+  id: string;
+  name: string;
+  public: boolean;
+  // Add more properties as per the Spotify API
+}
+
+interface SpotifyPlaylistsResponse {
+  items: SpotifyPlaylistItem[];
+}
+
+// Function to handle GET request
 export async function GET(request: Request) {
   console.log("Handling GET request for playlists");
 
   try {
     const session = await auth();
-    if (!session) {
-      return new Response("User must be authenticated", {
-        status: 401,
-      });
+    if (!session || !session.accessToken) {
+      // Ensure session and accessToken are available
+      return new Response(
+        "User must be authenticated and have a valid access token",
+        {
+          status: 401,
+        }
+      );
     }
 
-    const playlists = [
-      { id: 1, name: "Playlist 1" },
-      { id: 2, name: "Playlist 2" },
-      { id: 3, name: "Playlist 3" },
-    ];
-    return new Response(JSON.stringify(playlists), {
+    const spotifyResponse = await fetch(
+      "https://api.spotify.com/v1/me/playlists",
+      {
+        headers: {
+          Authorization: `Bearer ${session.accessToken}`, // Use the access token here
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!spotifyResponse.ok) {
+      const error: SpotifyApiError = await spotifyResponse.json();
+      throw new Error(`Failed to fetch playlists: ${error.message}`);
+    }
+
+    const data: SpotifyPlaylistsResponse = await spotifyResponse.json();
+    return new Response(JSON.stringify(data.items), {
       status: 200,
     });
   } catch (error: any) {
